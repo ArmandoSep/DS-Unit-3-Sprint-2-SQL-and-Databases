3087a3f0602362fa5ec7c81e3517ac582e7d1c26
ArmandoSep <sepulveda.armando97@gmail.com>
2020-07-15 17:55:48 -0500

    Did some PostgreSQL queries on the titanic DB

--- module2-sql-for-analysis/titanic_db.py
+++ module2-sql-for-analysis/titanic_db.py
@@ -30,7 +30,7 @@ print(type(cursor)) #> <class 'psycopg2.extensions.cursor'>
 
 # CREATE TABLE
 table_creation_query = """
--- DROP TABLE passengers;
+DROP TABLE passengers;
 CREATE TABLE IF NOT EXISTS passengers (
   id SERIAL PRIMARY KEY,
   survived integer,
@@ -52,6 +52,91 @@ list_of_tuples = list(df.to_records(index=True))
 
 insertion_query = "INSERT INTO passengers (id, survived, pclass, name, gender, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
 execute_values(cursor, insertion_query, list_of_tuples)
+
+
+# QUERIES
+
+# Query 1: How many passengers survived, and how many died?
+query = "SELECT count(id) as count, survived = 1 as survived from passengers GROUP BY survived;"
+result1 = cursor.execute(query)
+result1 = cursor.fetchall()
+print("RESULT 1: How many passengers survived, and how many died?", result1)
+
+# Query 2: How many passengers were in each class?
+query = "SELECT count(id) as count, pclass from passengers GROUP BY pclass;"
+result2 = cursor.execute(query)
+result2 = cursor.fetchall()
+print("RESULT 2: How many passengers were in each class?", result2)
+
+# Query 3: How many passengers survived/died within each class?
+query = "SELECT pclass, survived = 1 as survived, count(*) as count FROM passengers GROUP by survived, pclass ORDER BY pclass;"
+result3 = cursor.execute(query)
+result3 = cursor.fetchall()
+print("RESULT 3: How many passengers survived/died within each class?", result3)
+
+# Query 4: What was the average age of survivors vs nonsurvivors?
+query = "SELECT	survived = 1 as survived,	count(*) as count, avg(age) as average_age FROM passengers GROUP by survived;"
+result4 = cursor.execute(query)
+result4 = cursor.fetchall()
+print("RESULT 4: What was the average age of survivors vs nonsurvivors?", result4)
+
+# Query 5: What was the average age of each passenger class?
+query = "SELECT pclass,	avg(age) as average_age FROM passengers GROUP by pclass ORDER BY pclass;"
+result5 = cursor.execute(query)
+result5 = cursor.fetchall()
+print("RESULT 5: What was the average age of each passenger class?", result5)
+
+# Query 6: What was the average fare by passenger class? By survival?
+query = """
+SELECT 
+	pclass,
+	survived = 1 as survived,
+	count(*) as count,
+	avg(age) as average_age
+FROM passengers
+GROUP by survived, pclass 
+ORDER BY pclass;
+"""
+result6 = cursor.execute(query)
+result6 = cursor.fetchall()
+print("RESULT 6: What was the average fare by passenger class? By survival?", result6)
+
+# Query 7: How many siblings/spouses aboard on average, by passenger class? By survival?
+query = """
+SELECT 
+	pclass,
+	survived = 1 as survived,
+	count(*) as count,
+	round(avg(sib_spouse_count), 2) as average_sibspouse
+FROM passengers
+GROUP by survived, pclass 
+ORDER BY pclass
+"""
+result7 = cursor.execute(query)
+result7 = cursor.fetchall()
+print("RESULT 7: How many siblings/spouses aboard on average, by passenger class? By survival?", result7)
+
+# Query 8: How many parents/children aboard on average, by passenger class? By survival?
+query = """
+SELECT 
+	pclass,
+	survived = 1 as survived,
+	count(*) as count,
+	round(avg(parent_child_count), 2) as average_children
+FROM passengers
+GROUP by survived, pclass 
+ORDER BY pclass
+"""
+result8 = cursor.execute(query)
+result8 = cursor.fetchall()
+print("RESULT 8: How many parents/children aboard on average, by passenger class? By survival?", result8)
+
+# Query 9: Do any passengers have the same first name?
+query = "SELECT count(id) as count,	split_part(name, ' ', 2) first_name FROM passengers GROUP BY first_name ORDER BY count DESC;"
+result9 = cursor.execute(query)
+result9 = cursor.fetchall()
+print("RESULT 9: Do any passengers have the same first name?", result9)
+
 
 # SAVE THE RESULTS!
 connection.commit()
